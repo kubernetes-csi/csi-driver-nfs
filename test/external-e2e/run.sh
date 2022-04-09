07735614ffd8c8ecfa0ff50f4c570bed9ebd4fb5
andyzhangx <xiazhang@microsoft.com>
2022-04-09 14:15:48 +0000

    test: disable one external test

--- test/external-e2e/run.sh
+++ test/external-e2e/run.sh
@@ -50,6 +50,6 @@ setup_e2e_binaries
 trap print_logs EXIT
 
 ginkgo -p --progress --v -focus="External.Storage.*$DRIVER.csi.k8s.io" \
-       -skip='\[Disruptive\]' kubernetes/test/bin/e2e.test  -- \
+       -skip='\[Disruptive\]|new pod with same fsgroup skips ownership changes to the volume contents' kubernetes/test/bin/e2e.test  -- \
        -storage.testdriver=$PROJECT_ROOT/test/external-e2e/testdriver.yaml \
        --kubeconfig=$KUBECONFIG
