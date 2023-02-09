b01c9f86283242f261c2450617e91b4499aef841
andyzhangx <xiazhang@microsoft.com>
2023-02-09 02:49:57 +0000

    fix: buildx issue with provenance disabled

--- test/external-e2e/run.sh
+++ test/external-e2e/run.sh
@@ -20,8 +20,7 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel)
 DRIVER="test"
 
 install_ginkgo () {
-    apt update -y
-    apt install -y golang-ginkgo-dev
+    go install github.com/onsi/ginkgo/ginkgo@v1.14.0
 }
 
 setup_e2e_binaries() {
