01bc8c7ffee2254893daa732aca2730410d024a6
andyzhangx <xiazhang@microsoft.com>
2026-08-29 07:22:39 +0000

    chore: fix gofmt

--- pkg/nfs/tar_windows_test.go
+++ pkg/nfs/tar_windows_test.go
@@ -1,3 +1,5 @@
+//go:build windows
+
 /*
 Copyright 2024 The Kubernetes Authors.
 
@@ -13,8 +15,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
-//go:build windows
 
 package nfs
 
